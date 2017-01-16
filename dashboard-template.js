8feef8e67fb9cf52840573234a2bdb96266eb162
Francis Kabinoff <francis.kabinoff@gmail.com>
2017-01-15 21:28:55 -0500

    init

---
+++ dashboard-template.js
@@ -0,0 +1,42 @@
+/*
+ *    Fill in host and port for Qlik engine
+ */
+var prefix = window.location.pathname.substr( 0, window.location.pathname.toLowerCase().lastIndexOf( "/extensions" ) + 1 );
+
+var config = {
+	host: window.location.hostname,
+	prefix: prefix,
+	port: window.location.port,
+	isSecure: window.location.protocol === "https:"
+};
+//to avoid errors in workbench: you can remove this when you have added an app
+var app;
+require.config({
+	baseUrl: (config.isSecure ? "https://" : "http://" ) + config.host + (config.port ? ":" + config.port : "" ) + config.prefix + "resources"
+});
+
+require(["js/qlik"], function (qlik) {
+	window.qlik = qlik;
+	
+	qlik.setOnError( function (error) {
+		console.log(error);
+	});
+
+	
+
+	//callbacks -- inserted here --
+	//open apps -- inserted here --
+
+	//get objects -- inserted here --
+	//create cubes and lists -- inserted here --
+
+
+	if (app) {
+		app.getObject('CurrentSelections','CurrentSelections');
+		$(".filter-drawer-toggle").click(function() {
+			qlik.resize();
+		});
+	} else {
+		$(".current-selections-placeholder span").css("display", "inline-block");
+	}
+});
